833dfaac01364d2af464748560fbe921b5aa720c
顾强 <guqiang@ycode.cn>
2015-12-29 16:06:43 +0800

    section13

--- routes/index.js
+++ routes/index.js
@@ -389,6 +389,10 @@ module.exports = function(app) {
     });
 };
 
+app.use(function (req, res) {
+    res.render("404");
+});
+
 function checkLogin(req, res, next){
     if (!req.session.user){
         req.flash('error', '未登录');
