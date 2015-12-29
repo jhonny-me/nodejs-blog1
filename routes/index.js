355b5c4ff934dca7e5f1cdc454ac9d1c86f945ad
顾强 <guqiang@ycode.cn>
2015-12-29 16:03:31 +0800

    section12

--- routes/index.js
+++ routes/index.js
@@ -205,6 +205,15 @@ module.exports = function(app) {
         });
     });
 
+    app.get('/links', function (req, res) {
+        res.render('links', {
+            title: '友情链接',
+            user: req.session.user,
+            success: req.flash('success').toString(),
+            error: req.flash('error').toString()
+        });
+    });
+
     app.get('/u/:name', function (req, res) {
         var page = parseInt(req.query.q) || 1;
         if (page ==0){
